437c9f79c9d9fbf8945943937be39048fc6a317b
Harrison Chapman <hchap1@hotmail.com>
2025-01-18 09:19:17 +1000

    expanded setup script

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-ln -sf ~/dotfiles/hypr ~/.config/hypr
-ln -sf ~/dotfiles/waybar ~/.config/waybar
+rm -rf ~/hypr
+rm -rf ~/waybar
+ln -sf ~/dotfiles/hypr ~/.config
+ln -sf ~/dotfiles/waybar ~/.config
+sudo dnf install waybar
+sudo dnf install hyprpaper
+sudo dnf copr enable atim/starship
+sudo dnf install starship
+cp ~/dotfiles/starship.toml ~/.config/starship.toml
+echo 'eval "$(starship init bash)' >> ~/.bashrc
